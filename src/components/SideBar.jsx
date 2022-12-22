bd2e7828b7e30e4cc30447fb70ccdb4476b9bb50
herloncosta <herlon36@gmail.com>
2022-12-21 21:30:22 -0300

    sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,12 +7,6 @@ export function SideBar() {
     <aside id="sidebar">
       <img src={Profile} alt="foto de perfil Herlon Costa" />
       <p className="title">Desenvolvedor</p>
-      <p>informações</p>
-      <p>redes sociais</p>
-      <p>informações de contato</p>
-      <a href="" className="btn">
-        Download Currículo
-      </a>
     </aside>
   );
 }
